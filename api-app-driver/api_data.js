009a703a0ba511a7801223a80c9a185a35663d2e
Alan Albuquerque <alan@paggtaxi.com.br>
2016-07-20 10:16:11 -0300

    Colocando api de login na documentação

--- api-app-driver/api_data.js
+++ api-app-driver/api_data.js
@@ -1,8 +1,156 @@
 define({ "api": [
   {
     "type": "post",
+    "url": "/login-driver/",
+    "title": "Authenticate driver",
+    "name": "GetAuthenticate",
+    "group": "Authenticate",
+    "parameter": {
+      "examples": [
+        {
+          "title": "Request-Example:",
+          "content": "{\n    \"login\": \"00000000000\",\n    \"password\": \"123456\"\n}",
+          "type": "json"
+        }
+      ],
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "login",
+            "description": "<p>CPF of the driver</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "password",
+            "description": "<p>Password of driver (the same used to authenticate in the Paggtaxi)</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n    \"userId\": 9999,\n    \"token\": \"4dr-5c89e2fe5002fa5732e5\",\n    \"cpf\": \"00000000000\",\n    \"sellerId\": \"f89b46168b454fbbb449feff4ae4b255\",\n    \"userName\": \"John Richard\",\n    \"transactionShortName\": \"TAXI John\"\n    \"allowedPayments\": [\n        \"money\",\n        \"voucher\",\n        \"card\",\n        \"paggtaxi\"\n    ],\n    \"debitGateway\": \"stone\",\n    \"creditGateway\": \"stone\",\n    \"contractType\": 20,\n    \"profilePhoto\": \"http://site.com/media/4218-f1c97f222ad246d592580ba221a7404b.jpg\",\n    \"coopName\": \"Coop\",\n    \"errorMessage\": \"\",\n    \"success\": true,\n}",
+          "type": "json"
+        }
+      ],
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Integer",
+            "optional": false,
+            "field": "userId",
+            "description": "<p>Driver ID</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "token",
+            "description": "<p>Driver session token</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "cpf",
+            "description": "<p>CPF of the driver</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "sellerId",
+            "description": "<p>Seller ID of the driver to create Zoop transactions</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "userName",
+            "description": "<p>Full name of driver</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "transactionShortName",
+            "description": "<p>Name to show in card receipt</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Array",
+            "optional": false,
+            "field": "allowedPayments",
+            "description": "<p>Types of payment accepted by taxi driver</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Array",
+            "optional": false,
+            "field": "debitGateway",
+            "description": "<p>Acquirer to use on create debit transactions - enum(&quot;stone&quot;, &quot;zoop&quot;)</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Array",
+            "optional": false,
+            "field": "creditGateway",
+            "description": "<p>Acquirer to use on create credit transactions - enum(&quot;stone&quot;, &quot;zoop&quot;)</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Integer",
+            "optional": false,
+            "field": "contractType",
+            "description": "<p>Type of driver contract - enum(10, 20)</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Integer",
+            "optional": false,
+            "field": "contractType.10",
+            "description": "<p>Can use all application features.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Integer",
+            "optional": false,
+            "field": "contractType.20",
+            "description": "<p>Can use only card transactions.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "profilePhoto",
+            "description": "<p>URL of driver profile photo</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "coopName",
+            "description": "<p>Name of cab cooperative</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "/home/paggtaxi/myprojects/pagg_taxi/pagg_taxi/apps/api_paggtaxi_app/views/apidoc.py",
+    "groupTitle": "Authenticate"
+  },
+  {
+    "type": "post",
     "url": "/transactions/",
-    "title": "Get transactions by period",
+    "title": "List transactions by period",
     "name": "GetTransactions",
     "group": "Transactions",
     "parameter": {
@@ -20,14 +168,14 @@ define({ "api": [
             "type": "String",
             "optional": false,
             "field": "userId",
-            "description": "<p>Driver user id in Paggtaxi (Given in login request)</p>"
+            "description": "<p>Driver ID (given in login request)</p>"
           },
           {
             "group": "Parameter",
             "type": "String",
             "optional": false,
             "field": "token",
-            "description": "<p>Session token of driver user (Given in login request)</p>"
+            "description": "<p>Driver session token (given in login request)</p>"
           },
           {
             "group": "Parameter",
@@ -54,21 +202,28 @@ define({ "api": [
             "type": "Integer",
             "optional": false,
             "field": "userId",
-            "description": "<p>id of the User in Paggtaxi server</p>"
+            "description": "<p>Driver ID</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Integer",
+            "optional": false,
+            "field": "datetime_sync",
+            "description": "<p>Date of last sync DATETIME (ISO8601)</p>"
           },
           {
             "group": "Success 200",
             "type": "Boolean",
             "optional": false,
             "field": "success",
-            "description": "<p>Result of request</p>"
+            "description": "<p>Request results</p>"
           },
           {
             "group": "Success 200",
             "type": "Array",
             "optional": false,
             "field": "transactions",
-            "description": "<p>List containing the transactions</p>"
+            "description": "<p>List of transactions</p>"
           },
           {
             "group": "Success 200",
@@ -124,14 +279,14 @@ define({ "api": [
             "type": "String",
             "optional": false,
             "field": "transactions.precreate.payment_type",
-            "description": "<p>&quot;DEBIT&quot; or &quot;CREDIT&quot;</p>"
+            "description": "<p>enum('DEBIT', 'CREDIT')</p>"
           },
           {
             "group": "Success 200",
             "type": "String",
             "optional": false,
             "field": "transactions.precreate.provider",
-            "description": "<p>&quot;STONE&quot; or &quot;ZOOP&quot;</p>"
+            "description": "<p>enum('STONE', 'ZOOP')</p>"
           },
           {
             "group": "Success 200",
@@ -166,21 +321,21 @@ define({ "api": [
             "type": "String",
             "optional": false,
             "field": "transactions.voucher.voucher",
-            "description": "<p>Unique identifier of voucher</p>"
+            "description": "<p>Unique ID of voucher</p>"
           },
           {
             "group": "Success 200",
             "type": "Boolean",
             "optional": false,
             "field": "transactions.voucher.offline",
-            "description": "<p>This ride is created by cab cooperative operators?</p>"
+            "description": "<p>This ride was created by cab cooperative operators?</p>"
           },
           {
             "group": "Success 200",
             "type": "String",
             "optional": false,
             "field": "transactions.voucher.os",
-            "description": "<p>Unique identifier of ride in cab cooperative system</p>"
+            "description": "<p>Unique ID of ride in cab cooperative system</p>"
           },
           {
             "group": "Success 200",
@@ -215,14 +370,14 @@ define({ "api": [
             "type": "String",
             "optional": false,
             "field": "transactions.card_transaction.payment_type",
-            "description": "<p>&quot;DEBIT&quot; or &quot;CREDIT&quot;</p>"
+            "description": "<p>enum('DEBIT', 'CREDIT')</p>"
           },
           {
             "group": "Success 200",
             "type": "String",
             "optional": false,
             "field": "transactions.card_transaction.provider",
-            "description": "<p>&quot;STONE&quot; or &quot;ZOOP&quot;</p>"
+            "description": "<p>enum('STONE', 'ZOOP')</p>"
           },
           {
             "group": "Success 200",
@@ -250,20 +405,20 @@ define({ "api": [
             "type": "String",
             "optional": false,
             "field": "transactions.card_transaction.time_to_pass",
-            "description": "<p>Time to pass the transaction in the PIN pad</p>"
+            "description": "<p>Elapsed time to pass the transaction in the PIN Pad</p>"
           },
           {
             "group": "Success 200",
             "type": "String",
             "optional": false,
             "field": "transactions.card_transaction.transaction_status",
-            "description": "<p>&quot;CREATED&quot; or &quot;REVERSED&quot; or &quot;PRECREATED&quot;</p>"
+            "description": "<p>enum('CREATED', 'REVERSED', 'PRECREATED')</p>"
           }
         ]
       }
     },
     "version": "0.0.0",
-    "filename": "/home/paggtaxi/myprojects/pagg_taxi/pagg_taxi/apps/api_paggtaxi_app/views/transactions.py",
+    "filename": "/home/paggtaxi/myprojects/pagg_taxi/pagg_taxi/apps/api_paggtaxi_app/views/apidoc.py",
     "groupTitle": "Transactions",
     "error": {
       "fields": {
@@ -273,7 +428,7 @@ define({ "api": [
             "type": "String",
             "optional": false,
             "field": "errorMessage",
-            "description": "<p>Description of error</p>"
+            "description": "<p>Error description</p>"
           },
           {
             "group": "Error 4xx",
@@ -314,14 +469,14 @@ define({ "api": [
             "type": "String",
             "optional": false,
             "field": "userId",
-            "description": "<p>Driver user id in Paggtaxi (Given in login request)</p>"
+            "description": "<p>Driver ID (given in login request)</p>"
           },
           {
             "group": "Parameter",
             "type": "String",
             "optional": false,
             "field": "token",
-            "description": "<p>Session token of driver user (Given in login request)</p>"
+            "description": "<p>Driver session token (given in login request)</p>"
           },
           {
             "group": "Parameter",
@@ -353,27 +508,27 @@ define({ "api": [
             "type": "Integer",
             "optional": false,
             "field": "userId",
-            "description": "<p>id of the User in Paggtaxi server</p>"
+            "description": "<p>User ID</p>"
           },
           {
             "group": "Success 200",
             "type": "Boolean",
             "optional": false,
             "field": "success",
-            "description": "<p>Result of request</p>"
+            "description": "<p>Request results</p>"
           },
           {
             "group": "Success 200",
             "type": "Array",
             "optional": false,
             "field": "transactions",
-            "description": "<p>List containing the transaction information</p>"
+            "description": "<p>Transaction information</p>"
           }
         ]
       }
     },
     "version": "0.0.0",
-    "filename": "/home/paggtaxi/myprojects/pagg_taxi/pagg_taxi/apps/api_paggtaxi_app/views/transactions.py",
+    "filename": "/home/paggtaxi/myprojects/pagg_taxi/pagg_taxi/apps/api_paggtaxi_app/views/apidoc.py",
     "groupTitle": "Transactions",
     "error": {
       "fields": {
@@ -383,7 +538,7 @@ define({ "api": [
             "type": "String",
             "optional": false,
             "field": "errorMessage",
-            "description": "<p>Description of error</p>"
+            "description": "<p>Error description</p>"
           },
           {
             "group": "Error 4xx",
